754c67825a345f22af7462aadf9df6f5d2850300
Umer <umer@swishtag.com>
2022-03-11 19:48:21 +0500

    auth updated

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -77,7 +77,7 @@ class AuthController extends Controller
                 return response()->json([
                     'success' => false,
                     'message' => 'An error occured while registration!',
-                    'user' => null
+                    'user' => $customer
                 ]);
             }
             DB::commit();
