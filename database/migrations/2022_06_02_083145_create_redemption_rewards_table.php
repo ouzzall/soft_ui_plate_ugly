fc6d674cb71569ef58f9e161b7dc38dc0b5bf2cd
Abdullah Ayaz <atillawall@gmail.com>
2022-06-10 15:46:47 +0500

    change

--- database/migrations/2022_06_02_083145_create_redemption_rewards_table.php
+++ database/migrations/2022_06_02_083145_create_redemption_rewards_table.php
@@ -20,7 +20,7 @@ class CreateRedemptionRewardsTable extends Migration
             $table->bigInteger('prev_reward_id');
             $table->bigInteger('plan_id');
             $table->bigInteger('product_id');
-            $table->bigInteger('product_title');
+            $table->string('product_title');
             $table->bigInteger('variant_id');
             $table->string('image_src');
             $table->timestamps();
