f6303d2f79bb368159fc130cba80ac287923694e
Abdullah Ayaz <atillawall@gmail.com>
2022-06-20 14:04:02 +0500

    complete

--- app/Http/Controllers/RedemptionController.php
+++ app/Http/Controllers/RedemptionController.php
@@ -361,6 +361,8 @@ class RedemptionController extends Controller
             ->get();
         }
 
+        return array ($current_plan,$next_plan,$vbl3);
+
         $vbl4 = RedemptionReward::orderBy('prev_reward_id','asc')->get();
         $vbl7 = RewardRecieved::all();
 
