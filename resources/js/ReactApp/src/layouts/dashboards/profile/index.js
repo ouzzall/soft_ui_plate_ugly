71e1f0c70addf5691b603c287d54f0524796f67e
Abdullah Ayaz <atillawall@gmail.com>
2022-06-20 14:48:13 +0500

    complete

--- resources/js/ReactApp/src/layouts/dashboards/profile/index.js
+++ resources/js/ReactApp/src/layouts/dashboards/profile/index.js
@@ -127,6 +127,7 @@ function Profile() {
                             next_plan = response.data[0][i + 1];
                         console.log(current_plan);
                         console.log(next_plan);
+                        console.log(next_plan);
                         break;
                         // next_plan = response.data[0][i + 1];
                         // console.log(current_plan);
