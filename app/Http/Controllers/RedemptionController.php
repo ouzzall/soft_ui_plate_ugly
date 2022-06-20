7bb2adc7b54d9e6eef0635bd70e1577b3e79bae3
Abdullah Ayaz <atillawall@gmail.com>
2022-06-20 15:52:22 +0500

    changes

--- app/Http/Controllers/RedemptionController.php
+++ app/Http/Controllers/RedemptionController.php
@@ -339,7 +339,13 @@ class RedemptionController extends Controller
         {
             for ($i = 0; $i < count($vbl1); $i++) {
 
-                echo $vbl1[$i];
+                echo $vbl1[$i]->min_orders_amount;
+
+                $vbl3 = Order::where('user_id',$vbl2->id)
+                ->where('amount' , '>=' , $vbl1[$i]->min_orders_amount)
+                ->get();
+
+                echo count($vbl3);
 
                 // if (count($vbl3) >= $vbl1[$i]->orders) {
                 //     $current_plan = $vbl1[$i];
